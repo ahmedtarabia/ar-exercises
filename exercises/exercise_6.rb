3791ca1a9b521780702dc80422a6b01c5c9e5d7c
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-08 18:39:23 -0500

    add employees and stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,16 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: 'Khurram', last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Lionel", last_name: "Messi", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Cristiano", last_name: "Ronaldo", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Mohamed", last_name: "Salah", hourly_rate: 30)
+
+@store2.employees.create(first_name: "Sadio", last_name: "Mane", hourly_rate: 10)
+
+@store2.employees.create(first_name: "Francesco", last_name: "Totti", hourly_rate: 20)
+
+
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,20 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter your store name: "
+store_name = gets.chomp
+
+puts "Revenue Number: "
+store_revenue = gets.chomp
+
+puts "Men's Apparel: T/F"
+mens_apparel = gets.chomp
+
+puts "Women's Apparel: T/F"
+womens_apparel = gets.chomp
+
+new_store = Store.create(name: store_name, annual_revenue: store_revenue, mens_apparel: mens_apparel, womens_apparel: womens_apparel)
+
+
+p new_store.errors[:name]
+p new_store.errors[:annual_revenue]
