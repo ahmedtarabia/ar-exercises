bd2912e77ac46965fe95eb036aae080e713bdb16
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-08 17:51:01 -0500

    loop through  data to show select information

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,16 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store4 = Store.create(name: 'Surrey', annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+
+store5 = Store.create(name: 'Whistler', annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+
+store6 = Store.create(name: 'Yaletown', annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+puts "Men's Stores includes-----"
+@men_stores = Store.where(mens_apparel: true)
+@men_stores.each { |store| puts store.name, store.annual_revenue }
+
+puts "Women's store under a million includes------"
+@women_stores_under_1million =  Store.where('annual_revenue < 1000000').where(womens_apparel: true)
+@women_stores_under_1million.each { |store| puts store.name, store.annual_revenue }
